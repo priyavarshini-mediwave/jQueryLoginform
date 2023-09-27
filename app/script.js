06c5ec9b2923d12e869c9004c316ec1ee095344a
priyavarshini-mediwave <priyavarshini@mindwaveventures.com>
2023-09-27 17:04:03 +0530

    Write in as separate function

--- app/script.js
+++ app/script.js
@@ -1,33 +1,44 @@
+let profile = [];
 $(document).ready(function () {
-  const profile = [];
-  const btnSubmit = $("#submitbtn");
-  btnSubmit.click(function (e) {
-    const email = $("#email").val();
-    const password = $("#password").val();
-    if (!email) {
-      alert("Enter valid EmailId");
-    } else if (password.length < 8) {
-      alert("Password must be minimum 8 charcters ");
-    } else {
-      if (IsEmail(email) == true) {
-        if (IsPassword(password) == true) {
-          const item = {
-            id: new Date().getTime(),
-            email: email,
-            password: password,
-          };
-          profile.push(item);
-          console.log(profile);
-        } else {
-          alert(
-            "Invalid Password, Password must contain anyone of the following symbols !#$^ and alphabets"
-          );
-        }
+  
+  getfromLocal();
+  startfunction();
+});
+
+
+function startfunction (){
+const btnSubmit = $("#submitbtn");
+btnSubmit.click(function (e) {
+  const email = $("#email").val();
+  const password = $("#password").val();
+  //validateInputandDisplay(email,password)
+  if (!email) {
+    alert("Enter valid EmailId");
+  } else if (password.length < 8) {
+    alert("Password must be minimum 8 charcters ");
+  } else {
+    if (IsEmail(email) == true) {
+      if (IsPassword(password) == true) {
+        const item = {
+          id: new Date().getTime(),
+          email: email,
+          password: password,
+        };
+
+        profile.push(item);
+        console.log(profile);
+        savetoLocal();
       } else {
-        alert("Invalid EmailId");
+        alert(
+          "Invalid Password, Password must contain anyone of the following symbols !#$^ and alphabets and numbers"
+        );
       }
+    } else {
+      alert("Invalid EmailId");
     }
-  });
+  }
+});}
+  
   function IsEmail(email) {
     var regex =
       /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
@@ -45,6 +56,18 @@ $(document).ready(function () {
       return true;
     }
   }
-});
+  function savetoLocal() {
+    const str = JSON.stringify(profile);
+    localStorage.setItem("profile", str);
+  }
+  function getfromLocal() {
+    const str = localStorage.getItem("profile");
+    if (!str) {
+      profile = [];
+    } else {
+      profile = JSON.parse(str);
+    }
+  }
+
 
 //!#$^
